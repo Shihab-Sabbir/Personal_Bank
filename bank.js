a9629954d0a5337b5cf67ac65b68f2fdb7a21b37
shihab-sabbir <shihab11231@gmail.com>
2022-08-16 04:09:25 +0600

    4

--- bank.js
+++ bank.js
@@ -12,26 +12,32 @@ function dipositeAmmount() {
     const dipAmnt = parseFloat(document.getElementById('diposite_input').value);
     const currDiposite = parseFloat(document.getElementById('diposite').innerText);
     const oldBalace = parseFloat(document.getElementById('currentBalance').innerText);
-    document.getElementById('currentBalance').innerText = oldBalace + dipAmnt;
-    document.getElementById('diposite').innerText = currDiposite + dipAmnt;
-    window.localStorage.setItem('diposite', currDiposite + dipAmnt);
-    window.localStorage.setItem('currentBalance', oldBalace + dipAmnt);
-    document.getElementById('diposite_input').value = 0;
+    if (dipAmnt <= 0) { document.getElementById('diposite_input').value = 0; window.alert("Please Input Valid Amount"); }
+    else if (dipAmnt > 0) {
+        document.getElementById('currentBalance').innerText = oldBalace + dipAmnt;
+        document.getElementById('diposite').innerText = currDiposite + dipAmnt;
+        window.localStorage.setItem('diposite', currDiposite + dipAmnt);
+        window.localStorage.setItem('currentBalance', oldBalace + dipAmnt);
+        document.getElementById('diposite_input').value = 0;
+    }
 }
 function withrawAmmount() {
     const withAmnt = parseFloat(document.getElementById('withdraw_input').value);
     const currWithdraw = parseFloat(document.getElementById('withdraw').innerText);
     const oldBalace = parseFloat(document.getElementById('currentBalance').innerText);
-    if (withAmnt > oldBalace) {
-        window.alert('You can not withdraw more than current balance');
-        document.getElementById('withdraw_input').value = 0;
-    }
-    else if (withAmnt <= oldBalace) {
-        document.getElementById('withdraw').innerText = currWithdraw + withAmnt;
-        document.getElementById('currentBalance').innerText = oldBalace - withAmnt;
-        window.localStorage.setItem('withdraw', oldBalace - withAmnt);
-        window.localStorage.setItem('currentBalance', oldBalace - withAmnt);
-        document.getElementById('withdraw_input').value = 0;
+    if (withAmnt <= 0) { document.getElementById('withdraw_input').value = 0; window.alert("Please Input Valid Amount"); }
+    else if (withAmnt > 0) {
+        if (withAmnt > oldBalace) {
+            window.alert('You can not withdraw more than current balance');
+            document.getElementById('withdraw_input').value = 0;
+        }
+        else if (withAmnt <= oldBalace) {
+            document.getElementById('withdraw').innerText = currWithdraw + withAmnt;
+            document.getElementById('currentBalance').innerText = oldBalace - withAmnt;
+            window.localStorage.setItem('withdraw', currWithdraw + withAmnt);
+            window.localStorage.setItem('currentBalance', oldBalace - withAmnt);
+            document.getElementById('withdraw_input').value = 0;
+        }
     }
 }
 
